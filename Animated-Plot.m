1a99c872b62f3efe273d8fd08a5f03a43c69d89c
umangU <72771903+umangU@users.noreply.github.com>
2021-04-21 18:19:33 +1000

    Code Updated

--- Animated-Plot.m
+++ Animated-Plot.m
@@ -1,3 +1,4 @@
+#declaring x and y values
 format long
 y=@(t,x) exp(-t).*sin(2*x);
 t=0:0.1:10;
@@ -5,6 +6,7 @@ t=0:0.1:10;
 x=linspace(-2*pi,2*pi,length(t));
 comet3(t,x,y(t,x));
 
+#Animated Plot
 title('Animated plot');
 xlabel('t');
 ylabel('x');
